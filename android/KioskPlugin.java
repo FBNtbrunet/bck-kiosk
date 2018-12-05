234d512a53e478fb7c6e09f39863a7764fab12d4
FBNtbrunet <tbrunet@feedbacknow.com>
2018-12-05 18:16:28 +0100

    working state

--- android/KioskPlugin.java
+++ android/KioskPlugin.java
@@ -2,7 +2,6 @@ package jk.cordova.plugin.kiosk;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import org.apache.cordova.*;
 import android.widget.*;
@@ -17,15 +16,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import jk.cordova.plugin.kiosk.KioskActivity;
 import org.json.JSONObject;
-import java.lang.Integer;
-import java.util.HashSet;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 
 public class KioskPlugin extends CordovaPlugin {
     
     public static final String EXIT_KIOSK = "exitKiosk";
+    
     public static final String IS_IN_KIOSK = "isInKiosk";
-    public static final String IS_SET_AS_LAUNCHER = "isSetAsLauncher";
-    public static final String SET_ALLOWED_KEYS = "setAllowedKeys";
+
+    private static final String PREF_KIOSK_MODE = "pref_kiosk_mode";
+
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -35,34 +36,17 @@ public class KioskPlugin extends CordovaPlugin {
                 callbackContext.success(Boolean.toString(KioskActivity.running));
                 return true;
                 
-            } else if (IS_SET_AS_LAUNCHER.equals(action)) {
-                
-                String myPackage = cordova.getActivity().getApplicationContext().getPackageName();
-                callbackContext.success(Boolean.toString(myPackage.equals(findLauncherPackageName())));
-                return true;
-                
             } else if (EXIT_KIOSK.equals(action)) {
                 
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.addCategory(Intent.CATEGORY_HOME);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity().getApplicationContext());
+                sp.edit().putBoolean(PREF_KIOSK_MODE, false).commit();
+
                 Intent chooser = Intent.createChooser(intent, "Select destination...");
                 if (intent.resolveActivity(cordova.getActivity().getPackageManager()) != null) {
                     cordova.getActivity().startActivity(chooser);
                 }
-                
-                callbackContext.success();
-                return true;
-                
-            } else if (SET_ALLOWED_KEYS.equals(action)) {
-                
-                System.out.println("setAllowedKeys: " + args.toString());
-                HashSet<Integer> allowedKeys = new HashSet<Integer>();
-                for (int i = 0; i < args.length(); i++) {
-                    allowedKeys.add(args.optInt(i));
-                }
-                KioskActivity.allowedKeys = allowedKeys;
                 
                 callbackContext.success();
                 return true;
@@ -75,11 +59,5 @@ public class KioskPlugin extends CordovaPlugin {
             return false;
         }
     }
-    
-    private String findLauncherPackageName() {
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        final ResolveInfo res = this.cordova.getActivity().getPackageManager().resolveActivity(intent, 0);
-        return res.activityInfo.packageName;
-    }
 }
+
